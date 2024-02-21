2a155734d970bdfc19482c7e99dca7ff322eb25f
PandaCloud <134182273+pandacloud1@users.noreply.github.com>
2024-02-21 22:34:29 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our Online E-Commerce Store";
+		return "Welcome to our DevOps Project !!";
 	}
 
 	public static void main(String[] args) {
